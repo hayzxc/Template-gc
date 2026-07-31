510bc73fbcae8d87a498d974d63fd7ff855c155e
hayzxc <kurniawanhay@gmail.com>
2026-07-31 18:02:29 +0700

    chore: remove template assets and IDE agent configs from git tracking

--- _template_gc/kill3000.ps1
+++
@@ -1,7 +0,0 @@
-$conns = Get-NetTCPConnection -LocalPort 3000 -State Listen -ErrorAction SilentlyContinue
-if ($conns) {
-  $conns | ForEach-Object { Stop-Process -Id $_.OwningProcess -Force -ErrorAction SilentlyContinue }
-  Write-Output "stopped process(es) on port 3000"
-} else {
-  Write-Output "nothing listening on 3000"
-}
--- _template_gc/pretty.ps1
+++
@@ -1,7 +0,0 @@
-$x = [xml](Get-Content '_template_gc\word\document.xml' -Raw)
-$sw = New-Object System.IO.StringWriter
-$xw = New-Object System.Xml.XmlTextWriter($sw)
-$xw.Formatting = 'Indented'
-$x.WriteContentTo($xw)
-$sw.ToString() | Out-File '_template_gc\document_pretty.xml' -Encoding utf8
-(Get-Item '_template_gc\document_pretty.xml').Length
